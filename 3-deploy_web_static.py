186e9e7cf39d50982c762a2c188015a1d04cf917
Abu1914 <dmuktaru@gmail.com>
2024-04-10 06:57:51 -0700

    Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -27,6 +27,29 @@ def do_pack():
     return None
 
 
+def do_deploy(archive_path):
+    """deploy web_static using fabric"""
+    if not exists(archive_path):
+        return False
+    try:
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        print("New version deployed!")
+        flag = True
+    except Exception as e:
+        flag = False
+    return flag
+
+
 def deploy():
     """creates and deploys an archive to my servers"""
     archive = do_pack()
