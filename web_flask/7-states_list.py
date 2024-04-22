53dfaf9e278e527838176272a250a7dd2c6d08b5
Abu1914 <dmuktaru@gmail.com>
2024-04-22 13:44:03 +0100

    Correction

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-    Starts Flask web application
-    Web app listens on 0.0.0.0 port 5000
-    /hbnb page include
-    /c/<text> page included
-    /python/(<text>) page included
-    /number/<n> page included
-    /number_template/<n> page included
-    /number_odd_or_even/<n> page included
+Starts Flask web application
+Web app listens on 0.0.0.0 port 5000
+/hbnb page include
+/c/<text> page included
+/python/(<text>) page included
+/number/<n> page included
+/number_template/<n> page included
+/number_odd_or_even/<n> page included
 """
 from models import storage
 from models import *
